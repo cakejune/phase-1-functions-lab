f949e19775415e8df52ddc5467a938507c86da42
cakejune <tyq444@gmail.com>
2022-10-12 09:43:26 -0400

    commit tree c962a91c3755d92a234719fdd62c2ebbcefee56f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(originInBlocks, targetInBlocks) {
+    //returns the number of blocks given a value
+    const distanceinBlocks = targetInBlocks - originInBlocks;
+    return distanceinBlocks;    
+}
+function distanceFromHqInFeet(originInBlocks, targetInBlocks) {
+    
+    distanceFromHqInBlocks(originInBlocks, targetInBlocks);
+    const distanceInFeet = distanceInBlocks(originInBlocks, targetInBlocks) * 264;
+    return distanceInFeet;
+}
